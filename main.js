c244a5d72b8bcefffa50bc2b0a424e9f88965a58
Jonathan Rees <jrees@yellowtail.nl>
2024-02-10 16:06:36 +0200

    - add some more basic styling for Xs and Os - add some basic JS

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import './style.css'
+
+const squareEls = document.getElementsByClassName('square');
+
+for (let i = 0; i < squareEls.length; i++) {
+    const element = squareEls[i];
+    element.addEventListener('click', (_) => {
+        element.classList.add('x');
+    });
+}
